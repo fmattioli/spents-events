f1fe16c1bedad0051239105316e6bd716ce1bc93
Felipe Santos - SPCM <felipe.santos@SPCM.COM.BR>
2023-01-16 22:29:43 +0000

    First commit

---
+++ src/Spents.Events/Events/v1/ReceiptCreatedEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace Spents.Events.Events.v1
+{
+    public class ReceiptCreatedEvent
+    {
+        public DateTime EventCreatedDate { get; set; }
+        public string ReceiptName { get; set; } = null!;
+        public DateTime ReceiptDate { get; set; }
+        public IEnumerable<ReceiptItemsDetail> ReceiptItems { get; set; } = null!;
+    }
+
+    public class ReceiptItemsDetail
+    {
+        public string Name { get; set; } = null!;
+        public short Quantity { get; set; }
+        public decimal ItemPrice { get; set; }
+        public string Observation { get; set; } = null!;
+    }
+}
