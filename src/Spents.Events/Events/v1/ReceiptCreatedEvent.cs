f5bcbd090174929ae8891ade3b350454a3fea3cd
Felipe <felipe-mattioli98@hotmail.com>
2023-01-28 20:21:06 +0000

    FEAT: Add new field to event

--- src/Spents.Events/Events/v1/ReceiptCreatedEvent.cs
+++ src/Spents.Events/Events/v1/ReceiptCreatedEvent.cs
@@ -12,13 +12,14 @@
         }
 
         public string MessageKey { get; set; }
-        private DateTime CreatedDate { get; set; }
+        public DateTime CreatedDate { get; set; }
         public string Version { get; set; }
         public ReceiptCreated Body { get; set; }
     }
 
     public record ReceiptCreated
     {
+        public Guid Id { get; set; }
         public string EstablishmentName { get; set; } = null!;
         public DateTime ReceiptDate { get; set; }
         public IEnumerable<ReceiptItemsDetail> ReceiptItems { get; set; } = null!;
