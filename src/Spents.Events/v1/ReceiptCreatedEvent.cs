197485b38e3b799b0e61c706cd0b3a601f4a2b07
Felipe <felipe-mattioli98@hotmail.com>
2023-02-04 17:21:09 +0000

    FEAT:Changing project organiaztion

--- src/Spents.Events/v1/ReceiptCreatedEvent.cs
+++ src/Spents.Events/v1/ReceiptCreatedEvent.cs
@@ -1,23 +1,23 @@
 ﻿using Spents.Domain.Entities;
-using Spents.Events.Events.Body;
+using Spents.Events.Body;
 
-namespace Spents.Events.Events.v1
+namespace Spents.Events.v1
 {
     public record ReceiptCreatedEvent
     {
-        public ReceiptCreatedEvent(MessageBody<Receipt> body, string messageKey)
+        public ReceiptCreatedEvent(MessageBody<Receipt> receiptCreated, string messageKey)
         {
             EventName = nameof(ReceiptCreatedEvent);
             CreatedDate = DateTime.UtcNow;
             Version = "v1";
             MessageKey = messageKey;
-            Body = body;
+            ReceiptCreated = receiptCreated;
         }
 
         public string EventName { get; set; }
         public string MessageKey { get; set; }
         public DateTime CreatedDate { get; set; }
         public string Version { get; set; }
-        public MessageBody<Receipt> Body { get; set; }
+        public MessageBody<Receipt> ReceiptCreated { get; set; }
     }
 }
